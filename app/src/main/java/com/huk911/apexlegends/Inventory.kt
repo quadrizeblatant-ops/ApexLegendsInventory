b4f4d0474939eb7490e9815ef606e74ada9950a1
Den <enter.your.thoughts@gmail.com>
2026-08-03 14:12:24 +0200

    7.10 solved

---
+++ app/src/main/java/com/huk911/apexlegends/Inventory.kt
@@ -0,0 +1,140 @@
+package com.huk911.apexlegends
+
+import com.huk911.apexlegends.models.Consumable
+import com.huk911.apexlegends.models.Grenade
+import com.huk911.apexlegends.models.Item
+import com.huk911.apexlegends.models.Weapon
+
+class Inventory {
+
+    val backpack: MutableList<Item> = mutableListOf(
+        Weapon("R-301", Rarity.RARE, 14, 18),
+        Consumable("Syringe", Rarity.COMMON, 25),
+        Grenade("Arc Star", Rarity.RARE, 75),
+        Weapon("Kraber", Rarity.LEGENDARY, 150, 6),
+        Consumable("Med Kit", Rarity.RARE, 100)
+    )
+
+    var primaryWeapon: Weapon? = null
+    var secondaryWeapon: Weapon? = null
+    var health = 42
+    private var shownSlotNumber = 0
+
+    fun moveToNextItem(): Item? {
+        if (backpack.isEmpty()) {
+            return null
+        }
+        shownSlotNumber = shownSlotNumber + 1
+        if (shownSlotNumber > backpack.size - 1) {
+            shownSlotNumber = 0
+        }
+        return backpack[shownSlotNumber]
+    }
+
+    fun pickUp(newItem: Item) {
+        backpack.add(newItem)
+    }
+
+    fun dropShownItem(): Item? {
+        if (backpack.isEmpty()) {
+            return null
+        }
+        val droppedItem = backpack[shownSlotNumber]
+        backpack.removeAt(shownSlotNumber)
+        if (shownSlotNumber > backpack.size - 1) {
+            shownSlotNumber = 0
+        }
+        return droppedItem
+    }
+
+    fun useShownItem(): String {
+        if (backpack.isEmpty()) {
+            return "Нечего использовать"
+        }
+        val chosenItem = backpack[shownSlotNumber]
+        return when (chosenItem) {
+            is Consumable -> {
+                if (health >= 100) {
+                    "HP уже 100, лечиться незачем"
+                } else {
+                    health = health + chosenItem.healAmount
+                    if (health > 100) {
+                        health = 100
+                    }
+                    chosenItem.name + ": +" + chosenItem.healAmount + " HP"
+                }
+            }
+
+            is Grenade -> {
+                health = health - chosenItem.blastDamage
+                "Вы подорвались на " + chosenItem.blastDamage + " урона"
+            }
+
+            else -> "Это нельзя использовать"
+        }
+    }
+
+    fun equipShownWeapon(): Boolean {
+        if (backpack.isEmpty()) {
+            return false
+        }
+        val chosenItem = backpack[shownSlotNumber]
+        if (chosenItem is Weapon) {
+            backpack.removeAt(shownSlotNumber)
+            val previousWeapon = primaryWeapon
+            if (previousWeapon != null) {
+                backpack.add(previousWeapon)
+            }
+            primaryWeapon = chosenItem
+            if (shownSlotNumber > backpack.size - 1) {
+                shownSlotNumber = 0
+            }
+            return true
+        }
+        return false
+    }
+
+    fun equipShownWeaponSecondary(): Boolean {
+        if (backpack.isEmpty()) {
+            return false
+        }
+        val chosenItem = backpack[shownSlotNumber]
+        if (chosenItem is Weapon) {
+            backpack.removeAt(shownSlotNumber)
+            val previousWeapon = secondaryWeapon
+            if (previousWeapon != null) {
+                backpack.add(previousWeapon)
+            }
+            secondaryWeapon = chosenItem
+            if (shownSlotNumber > backpack.size - 1) {
+                shownSlotNumber = 0
+            }
+            return true
+        }
+        return false
+    }
+
+    fun dropPrimary() {
+        primaryWeapon = null
+    }
+
+    fun dropSecondary() {
+        secondaryWeapon = null
+    }
+
+    fun swapHands() {
+        val temporaryWeapon = primaryWeapon
+        primaryWeapon = secondaryWeapon
+        secondaryWeapon = temporaryWeapon
+    }
+
+    fun calculateTotalValue(): Int {
+        var totalValue = 0
+        for (item in backpack) {
+            totalValue = totalValue + item.calculateValue()
+        }
+        val primaryValue = primaryWeapon?.calculateValue() ?: 0
+        val secondaryValue = secondaryWeapon?.calculateValue() ?: 0
+        return totalValue + primaryValue + secondaryValue
+    }
+}
--- app/src/main/java/com/huk911/apexlegends/MainActivity.kt
+++ app/src/main/java/com/huk911/apexlegends/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.huk911.apexlegends
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -10,9 +9,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.huk911.apexlegends.models.Consumable
-import com.huk911.apexlegends.models.Grenade
-import com.huk911.apexlegends.models.Item
-import com.huk911.apexlegends.models.Weapon
 import android.graphics.Color
 
 class MainActivity : AppCompatActivity() {
@@ -24,20 +20,10 @@ class MainActivity : AppCompatActivity() {
 
         applyInsets()
 
+        val inventory = Inventory()
+
         val itemCard: TextView = findViewById(R.id.itemCard)
         val nextItemButton: Button = findViewById(R.id.btn_next_item)
-
-        val r301 = Weapon("R-301", Rarity.RARE, 14, 18)
-        val syringe = Consumable("Syringe", Rarity.COMMON, 25)
-        val arcStar = Grenade("Arc Star", Rarity.RARE, 75)
-        val kraber = Weapon("Kraber", Rarity.LEGENDARY, 150, 6)
-        val medKit = Consumable("Med Kit", Rarity.RARE, 100)
-        val primary = Weapon("R-99", Rarity.RARE, 11, 18)
-        val secondary = Weapon("Wingman", Rarity.EPIC, 45, 6)
-
-        var primaryWeapon: Weapon? = null
-        var secondaryWeapon: Weapon? = null
-
         val healthBarCard = findViewById<TextView>(R.id.tv_health_bar)
         val hpInfoCard = findViewById<TextView>(R.id.tv_hp_info)
         val useButton = findViewById<Button>(R.id.btn_use)
@@ -53,196 +39,110 @@ class MainActivity : AppCompatActivity() {
         val equipButton = findViewById<Button>(R.id.btn_equip)
         val dropHandButton = findViewById<Button>(R.id.btn_drop_from_hand)
         val dropSecHandButton = findViewById<Button>(R.id.btn_drop_from_sec_hand)
-        val eqiupSecButton = findViewById<Button>(R.id.btn_equip_sec_hand)
-        val backpack: MutableList<Item> = mutableListOf(r301, syringe, arcStar, kraber, medKit)
+        val equipSecButton = findViewById<Button>(R.id.btn_equip_sec_hand)
 
-        val firstItem = backpack[0]
-        val itemCount = backpack.size
-
-
-        var health = 42
-        healthBarCard.text = "HP: $health"
-        var shownSlotNumber = 0
-        var currentItem: Item = backpack[0]
-
-        val handDescription = primaryWeapon?.toString()               // Рука
-        val handText = handDescription ?: "В руках пусто"
-        val previousWeapon = primaryWeapon
-        handCard.text = handText
-        if (previousWeapon != null) {
-            backpack.add(previousWeapon)
-        }
+        healthBarCard.text = "HP: " + inventory.health
+        handCard.text = inventory.primaryWeapon?.toString() ?: "В руках пусто"
+        secHandCard.text = inventory.secondaryWeapon?.toString() ?: "Во втором слоте пусто"
 
         dropHandButton.setOnClickListener {
-            if (primaryWeapon == null) {
+            if (inventory.primaryWeapon == null) {
                 handCard.text = "В руках пусто, нечего дропать"
             } else {
-                primaryWeapon = null
+                inventory.dropPrimary()
                 handCard.text = "В руках пусто"
             }
         }
 
         dropSecHandButton.setOnClickListener {
-            if (secondaryWeapon == null) {
+            if (inventory.secondaryWeapon == null) {
                 secHandCard.text = "Во втором слоте пусто, нечего дропать"
             } else {
-                secondaryWeapon = null
+                inventory.dropSecondary()
                 secHandCard.text = "Во втором слоте пусто"
             }
         }
 
         equipButton.setOnClickListener {
-            if (backpack.isEmpty()) {
+            if (inventory.backpack.isEmpty()) {
                 handCard.text = "В рюкзаке пусто, нечего эквипать"
             } else {
-                val chosenItem = backpack[shownSlotNumber]
-                if (chosenItem is Weapon) {
-                    backpack.removeAt(shownSlotNumber)
-                    val previousWeapon = primaryWeapon
-                    if (previousWeapon != null) {
-                        backpack.add(previousWeapon)
-                    }
-                    primaryWeapon = chosenItem
-                    if (shownSlotNumber > backpack.size - 1) {
-                        shownSlotNumber = 0
-                    }
-                    handCard.text = primaryWeapon?.toString() ?: "В руках пусто"
+                val isEquipped = inventory.equipShownWeapon()
+                if (isEquipped) {
+                    handCard.text = inventory.primaryWeapon?.toString() ?: "В руках пусто"
                 } else {
                     handCard.text = "В руки можно взять только одно оружие"
                 }
             }
         }
 
-        eqiupSecButton.setOnClickListener {
-            if (backpack.isEmpty()) {
+        equipSecButton.setOnClickListener {
+            if (inventory.backpack.isEmpty()) {
                 secHandCard.text = "В рюкзаке пусто, нечего эквипать"
             } else {
-                val chosenItem = backpack[shownSlotNumber]
-                if (chosenItem is Weapon) {
-                    backpack.removeAt(shownSlotNumber)
-                    val previousWeapon = secondaryWeapon
-                    if (previousWeapon != null) {
-                        backpack.add(previousWeapon)
-                    }
-                    secondaryWeapon = chosenItem
-                    if (shownSlotNumber > backpack.size - 1) {
-                        shownSlotNumber = 0
-                    }
-                    secHandCard.text = secondaryWeapon?.toString() ?: "Во втором слоте пусто"
+                val isEquipped = inventory.equipShownWeaponSecondary()
+                if (isEquipped) {
+                    secHandCard.text = inventory.secondaryWeapon?.toString() ?: "Во втором слоте пусто"
                 } else {
                     secHandCard.text = "В руки можно взять только одно оружие"
                 }
             }
         }
 
+        switchWeaponButton.setOnClickListener {
+            inventory.swapHands()
+            handCard.text = inventory.primaryWeapon?.toString() ?: "В руках пусто"
+            secHandCard.text = inventory.secondaryWeapon?.toString() ?: "Во втором слоте пусто"
+        }
+
         openInventory.setOnClickListener {
             var backpackText = ""
             var backpackNumber = 0
-            for (item in backpack) {
+            for (item in inventory.backpack) {
                 backpackNumber += 1
                 backpackText = backpackText + backpackNumber + ". " + item + "\n"
             }
-            val shownText = if (backpack.isEmpty()) "Инвентарь пуст" else backpackText
+            val shownText = if (inventory.backpack.isEmpty()) "Инвентарь пуст" else backpackText
             inventoryText.text = shownText
         }
 
         nextItemButton.setOnClickListener {
-            if (backpack.isEmpty()) {
-                itemCard.text = "Инвентарь пуст"
-            } else {
-                shownSlotNumber += 1
-                if (shownSlotNumber > backpack.size - 1) {
-                    shownSlotNumber = 0
-                }
-                val shownItem = backpack[shownSlotNumber]
+            val shownItem = inventory.moveToNextItem()
+            if (shownItem != null) {
                 itemCard.text = shownItem.toString()
                 val rarityColor = pickRarityColor(shownItem.rarity)
                 itemCard.setTextColor(rarityColor)
-                Log.i("govno", "тотал хп $health")
-                currentItem = shownItem
+            } else {
+                itemCard.text = "Инвентарь пуст"
             }
         }
 
         useButton.setOnClickListener {
-            if (backpack.isEmpty()) {
-                hpInfoCard.text = "Нечего использовать"
-            } else {
-                val capturedItem = currentItem
-                when (capturedItem) {
-                    is Consumable -> {
-                        if (health + capturedItem.healAmount < 100) {
-                            health += capturedItem.healAmount
-                            setHealthValue(healthBarCard, health)
-                            val healCard =
-                                capturedItem.name + ": +" + capturedItem.healAmount + " HP"
-                            hpInfoCard.text = healCard
-                        } else {
-                            health = 100
-                            healthBarCard.text = "HP: 100, вы не можете лечиться"
-                        }
-                    }
-
-                    is Grenade -> {
-                        health -= capturedItem.blastDamage
-                        setHealthValue(healthBarCard, health)
-                        hpInfoCard.text =
-                            "Вы подвзворвались на " + capturedItem.blastDamage + " урона"
-                    }
-                }
-                Log.i("govno", "тотал хп $health")
-            }
+            val useMessage = inventory.useShownItem()
+            hpInfoCard.text = useMessage
+            setHealthValue(healthBarCard, inventory.health)
         }
-
-        switchWeaponButton.setOnClickListener {
-            if (backpack.isEmpty()) {
-                itemCard.text = "Инвентарь пуст"
-            } else {
-                var weaponNumber = 0
-                weaponNumber++
-                if (weaponNumber == 3) weaponNumber = 0
-                val current = when (weaponNumber) {
-                    0 -> backpack[0]
-                    1 -> backpack[1]
-                    else -> 0
-                }
-                val card = current.toString()
-                itemCard.text = card
-                Log.i("govno", "button clicked with number $weaponNumber")
-            }
-        }
-
 
         pickButton.setOnClickListener {
             val shieldCell = Consumable("Shield Cell", Rarity.COMMON, 25)
-            backpack.add(shieldCell)
-            val itemCount = backpack.size
+            inventory.pickUp(shieldCell)
+            val itemCount = inventory.backpack.size
             itemCard.text = "Подобрано " + shieldCell.name + ". Предметов в инвентаре: " + itemCount
         }
 
         dropButton.setOnClickListener {
-            if (backpack.isEmpty()) {
-                itemCard.text = "Инвентарь пуст"
+            val droppedItem = inventory.dropShownItem()
+            if (droppedItem != null) {
+                itemCard.text = "Предмет " + droppedItem + " выброшен"
             } else {
-                var itemToDrop = backpack[shownSlotNumber]
-                backpack.removeAt(shownSlotNumber)
-                itemCard.text = "Предмет " + itemToDrop.toString() + " Выброшен нахуй"
-                if (shownSlotNumber > 0) {
-                    shownSlotNumber -= 1
-                }
+                itemCard.text = "Инвентарь пуст"
             }
-
         }
 
         valueButton.setOnClickListener {
-            var backpackValue = 0
-            val handValue = primary.calculateValue() + secondary.calculateValue()
-            for (item in backpack) {
-                backpackValue += item.calculateValue()
-            }
-            valueCard.text = "Ценность предметов в руках: " + handValue + "\n" +
-                    "Ценность инвентаря: " + backpackValue
+            val totalValue = inventory.calculateTotalValue()
+            valueCard.text = "Полная ценность инвентаря: " + totalValue
         }
-
     }
 
     private fun setHealthValue(healthBarCard: TextView, health: Int) {
